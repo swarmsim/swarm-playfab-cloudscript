1bc06af7f440b54ab1f2d3006e0e8322d9e7a77c
Evan <github@erosson.org>
2017-03-26 22:30:18 +0200

    fix internaldata

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 handlers.helloWorld = function(args, context) {
   var env = server.GetTitleInternalData({});
-  var message = "Hello " + currentPlayerId + "! inputvalue: " + (args || {}).inputValue + "; titleInternalData.ENV: " + env.ENV;
+  var message = "Hello " + currentPlayerId + "! inputvalue: " + (args || {}).inputValue + "; titleInternalData.ENV: " + env.Data.ENV;
   log.info(message);
   return { messageValue: message };
 };
