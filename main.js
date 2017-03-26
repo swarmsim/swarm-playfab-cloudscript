75f2e3c44d7fcb757ee6716155e3cb306ca065e8
Evan <github@erosson.org>
2017-03-26 23:36:43 +0200

    format paypal better

--- main.js
+++ main.js
@@ -5,6 +5,19 @@ handlers.helloWorld = function(args, context) {
   return { messageValue: message };
 };
 
+// This would be trivial with lodash/jquery
+// Careful to encodeURIComponent everything to avoid injection
+function formUrlencoded(obj) {
+  var ret = [];
+  for (key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      var val = obj[key];
+      ret.push(encodeURIComponent(key) + '=' + encodeURIComponent(val));
+    }
+  }
+  return ret.join('&');
+}
+
 // Paypal PDT payments.
 handlers.paypalNotify = function(args, context) {
   var env = server.GetTitleInternalData({});
@@ -12,10 +25,11 @@ handlers.paypalNotify = function(args, context) {
   var url = host + '/cgi-bin/webscr'
   var paypalIdentityToken = env.Data.PAYPAL_IDENTITY_TOKEN;
   var paypalTransactionId = args.tx;
-  var response = http.request(url, "post", JSON.stringify({
+
+  var response = http.request(url, "post", formUrlencoded({
     cmd: '_notify-sync',
     tx: paypalTransactionId,
     at: paypalIdentityToken,
-  }), 'application/json');
+  })), 'application/x-www-form-urlencoded');
   return {paypalResponse: response}
 };
